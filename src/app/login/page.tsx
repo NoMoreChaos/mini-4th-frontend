21b6e509cc5ca7864b70913c13ed8a44b6093b6a
roel <ash49901@gmail.com>
2025-12-05 12:00:26 +0900

    [FEAT/#3] 로그인 페이지 UI 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,20 @@
 "use client";
+
 import {NextPage} from "next";
+import {Box} from "@mui/material";
+import LoginCard from "@/app/login/component/LoginCard";
 
 const LoginPage: NextPage = () => {
     return (
-        <div>
-            Login Page
-            <textarea>123</textarea>
-            <button>확인</button>
-        </div>
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            sx={{backgroundColor: '#f5f5f5'}}
+        >
+            <LoginCard/>
+        </Box>
     )
 }
 
