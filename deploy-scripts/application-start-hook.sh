b215e1bbb2e3eaebbe868415761f028f19ce0e5f
roel <ash49901@gmail.com>
2025-12-23 11:01:41 +0900

    [HOTFIX] application-start-hook 수정

--- deploy-scripts/application-start-hook.sh
+++ deploy-scripts/application-start-hook.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 set -e
 
-export NVM_DIR="/home/ubuntu/.nvm"
-source "$NVM_DIR/nvm.sh"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install 25.2.1
 nvm use 25.2.1
